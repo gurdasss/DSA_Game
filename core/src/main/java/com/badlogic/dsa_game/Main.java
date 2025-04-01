1b00e7fb7b4942da7943c84e8dfc5609c352a443
Gurdas Satwani <gurdassatwani1@gmail.com>
2025-04-01 11:59:03 +0530

    Added some default libGDX default UI skins and other related files.

--- core/src/main/java/com/badlogic/dsa_game/Main.java
+++ core/src/main/java/com/badlogic/dsa_game/Main.java
@@ -4,6 +4,7 @@ import com.badlogic.gdx.ApplicationListener;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Stack;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 
@@ -25,7 +26,8 @@ public class Main implements ApplicationListener {
         Stack windowsStack = new Stack();
         windowsStack.setFillParent(true);
 
-//        Skin skin = new Skin(/* pass your uiskin.json */);
+        Skin skin = new Skin(Gdx.files.internal("uiskin.json"));
+
 
         stage.addActor(windowsStack);
 
