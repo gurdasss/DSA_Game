60bb08aefaeb61a380eb1a385c8888333aada767
Gurdas Satwani <gurdassatwani1@gmail.com>
2025-05-18 14:14:55 +0530

    Finally, completed a queue problem screen

--- core/src/main/java/com/badlogic/dsa_game/Main.java
+++ core/src/main/java/com/badlogic/dsa_game/Main.java
@@ -7,7 +7,7 @@ public class Main extends Game {
     @Override
     public void create() {
         // set the initial Game screen to Main Menu Screen
-        this.setScreen(new StackProblem(this));
+        this.setScreen(new QueueProblem(this));
     }
 
 }
---
+++ core/src/main/java/com/badlogic/dsa_game/QueueProblem.java
@@ -0,0 +1,98 @@
+package com.badlogic.dsa_game;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.scenes.scene2d.Actor;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.Touchable;
+import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
+import com.badlogic.gdx.utils.Queue;
+
+public class QueueProblem extends ProblemScreenConfig {
+
+    public QueueProblem(Game game) {
+        super(game);
+
+        DragAndDrop dragAndDrop = new DragAndDrop();
+
+        Queue<Actor> dataElementQueue = new Queue<>();
+
+
+        for (int i = 0; i < 5; i++) {
+            DataElement item = new DataElement(Integer.toString(i), skin);
+
+            dragAndDrop.addSource(new DragAndDrop.Source(item) {
+                @Override
+                public DragAndDrop.Payload dragStart(InputEvent event, float x, float y, int pointer) {
+                    DragAndDrop.Payload payload = new DragAndDrop.Payload();
+
+                    payload.setDragActor(getActor());
+                    payload.setObject(item.getDataValue());
+
+
+                    dragAndDrop.setDragActorPosition(
+                        getActor().getWidth() / 2f,
+                        -getActor().getHeight()
+                    );
+
+                    return payload;
+                }
+
+                @Override
+                public void dragStop(InputEvent event, float x, float y, int pointer, DragAndDrop.Payload payload, DragAndDrop.Target target) {
+                    if (target == null) {
+
+                        // if queue is not empty
+                        if (!dataElementQueue.isEmpty()) {
+                            // then remove the first element from the queue
+                            dataElementQueue.removeFirst();
+
+                            // if queue is not empty
+                            if (!dataElementQueue.isEmpty()) {
+                                // set newly first element to enable
+                                dataElementQueue.first().setTouchable(Touchable.enabled);
+                            }
+                        }
+
+                        sourceContainer.add(payload.getDragActor());
+                    }
+
+                }
+            });
+
+            sourceContainer.add(item);
+        }
+
+        dragAndDrop.addTarget(new DragAndDrop.Target(targetContainer) {
+            @Override
+            public boolean drag(DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
+                return true;
+            }
+
+
+            @Override
+            public void drop(DragAndDrop.Source source, DragAndDrop.Payload payload, float x, float y, int pointer) {
+
+                Actor currentDataElement = payload.getDragActor();
+
+                // check if the queue is not empty
+                if (!dataElementQueue.isEmpty()) {
+                    // if target container's first element is equal to queue's first element
+                    if (targetContainer.getChildren().first().equals(dataElementQueue.first())) {
+                        // then enable that element
+                        targetContainer.getChildren().first().setTouchable(Touchable.enabled);
+                    } else {
+                        // otherwise set the back element to disable
+                        currentDataElement.setTouchable(Touchable.disabled);
+                    }
+                }
+
+
+                targetContainer.add(currentDataElement);
+                dataElementQueue.addLast(currentDataElement);
+
+            }
+        });
+
+
+    }
+}
